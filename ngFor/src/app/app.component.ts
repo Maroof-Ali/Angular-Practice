c0c0c01fff7f8b796dbe98db1749a8b5932e3384
Maroof Ali <maroof.cyber@yahoo.com>
2020-10-29 12:58:03 +0500

    array output with ngFor

---
+++ ngFor/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  
+  numbers = [12,3,4,14,6,52,55,10,39,36]
+  
+
+}
