ac25960e9c52ae800aef6bf1d310707f502ee912
Maroof Ali <maroof.cyber@yahoo.com>
2020-11-01 03:39:24 +0500

    render and host listener

---
+++ creating-directive/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HighlightDirective } from './highlight-directive/highlight.directive';
+import { BetterHighlightDirective } from './better-highlight-directive/better-highlight.directive';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HighlightDirective,
+    BetterHighlightDirective
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ creating-directive/src/app/better-highlight-directive/better-highlight.directive.ts
@@ -0,0 +1,24 @@
+import { Directive, ElementRef, HostListener, OnInit, Renderer2 } from '@angular/core';
+
+@Directive({
+  selector: '[appBetterHighlight]'
+})
+export class BetterHighlightDirective implements OnInit{
+
+  constructor(private elementRef : ElementRef , private renderer : Renderer2) { }
+
+  ngOnInit(){
+    //this.renderer.setStyle(this.elementRef.nativeElement,'background-color','blue');
+  }
+
+  @HostListener('mouseenter') mouseover(eventData : Event){
+    this.renderer.setStyle(this.elementRef.nativeElement,'background-color','LightBlue');
+
+  }
+
+  @HostListener('mouseleave') mouseleave(eventData : Event){
+    this.renderer.setStyle(this.elementRef.nativeElement,'background-color','transparent');
+
+  }
+
+}
